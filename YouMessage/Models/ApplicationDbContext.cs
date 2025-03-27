d7334fb8ca6407c9e1f4a4b6b6a0f412ee078d20
matthew <matthew.birbeck@gmail.com>
2025-03-28 11:33:18 +1300

    Initial commit for YouMessage project
    - Set up ASP.NET Core Web App (Model-View-Controller) template
    - Added SignalR for real-time messaging
    - Configured Entity Framework Core with SQL Server
    - Implemented basic project structure with folders for Controllers, Models, Views, wwwroot, SignalR, and Migrations
    - Created initial files: Program.cs, Startup.cs, appsettings.json
    - Added Message model and ApplicationDbContext
    - Created ChatHub for SignalR
    - Implemented HomeController and Index view for chat interface
    - Added basic styling and layout

---
+++ YouMessage/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace YouMessage.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
